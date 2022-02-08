9f705a7669c20e0db847c342198d476f790dd15f
Alexey Revo <Scarlatum@yandex.ru>
2022-02-08 10:57:58 +0800

    Addition fix

--- source/plugins/Supabase.ts
+++ source/plugins/Supabase.ts
@@ -1,6 +1,6 @@
 
-import Vue from 'vue';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
+  import Vue from 'vue';
+  import { createClient, SupabaseClient } from '@supabase/supabase-js';
   
   const SUPA_URL = 'https://unfruhyobjypfbvnncoc.supabase.co';
   const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyOTQ4MDE0LCJleHAiOjE5NTg1MjQwMTR9.vmYoNzgkXoN0tUr8_q8xbsBbqrOsA0Dy5_pRsOG4TN8';
