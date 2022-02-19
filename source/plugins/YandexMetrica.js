22413c640c2f0b3fcba1bf890329f8170eab22ab
Alexey Revo <Scarlatum@yandex.ru>
2022-02-19 14:09:33 +0800

    [ Fix ]: YandexMetrica ID Update

--- source/plugins/YandexMetrica.js
+++ source/plugins/YandexMetrica.js
@@ -5,13 +5,11 @@ import VueYandexMetrika from 'vue-yandex-metrika'
 export default (context) => {
 
 	window.onload = function() {
-		setTimeout(() => {
-			Vue.use(VueYandexMetrika, {
-				id: 84655327,
-				router: context.app.router,
-				env: process.env.NODE_ENV,
-			})
-		}, 5000)
+		Vue.use(VueYandexMetrika, {
+			id: 87558820,
+			router: context.app.router,
+			env: process.env.NODE_ENV,
+		})
 	}
 
 }
