023106f2d691452501ad5e39563b365f01aa0e94
Alexey Revo <Scarlatum@yandex.ru>
2022-02-19 13:58:16 +0800

    [ Changes ]: Add yandex meta

--- properties/defaultHead.ts
+++ properties/defaultHead.ts
@@ -1,7 +1,12 @@
-import type { MetaInfo } from 'vue-meta';
+import type { MetaInfo, MetaPropertyName } from 'vue-meta';
+
+const yandexVerification: MetaPropertyName = { name: "yandex-verification", content: "cfc2699abdb958a8" }
 
 const HEAD: MetaInfo = {
   title: 'Eccheuma',
+  meta: [
+    yandexVerification,
+  ],
   link: [
     { rel: 'icon', href: './icon.svg' }
   ]
