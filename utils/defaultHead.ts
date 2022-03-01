ed9997267f6d0a107986b077fb94061dc236019b
Alexey Revo <Scarlatum@yandex.ru>
2022-03-02 03:45:57 +0800

    [ Addition ]: Viewport meta

--- utils/defaultHead.ts
+++ utils/defaultHead.ts
@@ -2,10 +2,16 @@ import type { MetaInfo, MetaPropertyName } from 'vue-meta';
 
 const yandexVerification: MetaPropertyName = { name: "yandex-verification", content: "cfc2699abdb958a8" };
 
+// <meta name="viewport" content="width=device-width, initial-scale=1">
+
 const HEAD: MetaInfo = {
   title: 'Eccheuma',
   meta: [
+
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
     yandexVerification,
+    
   ],
   link: [
     { rel: 'icon', href: './icon.svg' }
