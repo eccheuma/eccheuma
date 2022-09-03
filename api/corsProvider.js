74e705eccbf4fb44006ab6020b4a1df8eea3f5e1
Alexey Skurlatov <Scarlatum@yandex.ru>
2022-09-03 17:10:21 +0800

    CL

--- api/corsProvider.js
+++ api/corsProvider.js
@@ -4,6 +4,8 @@ export default async function handler(req, res) {
   
   const response = await nodeFetch(req.query.url)
   
+  console.log(response);
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
   res.end(JSON.stringify({ response: response }))
